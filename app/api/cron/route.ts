b8fd3dd757f83899e3e4c370013e278f92efafe5
paraj <bhattparaj2@gmail.com>
2024-06-26 20:38:59 +0530

    9th error resolved

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -1,6 +1,11 @@
 import { NextResponse } from "next/server";
 
-import { getLowestPrice, getHighestPrice, getAveragePrice, getEmailNotifType } from "@/lib/utils";
+import {
+  getLowestPrice,
+  getHighestPrice,
+  getAveragePrice,
+  getEmailNotifType,
+} from "@/lib/utils";
 import Product from "@/lib/models/product.model";
 import { scrapeAmazonProduct } from "@/lib/scraper";
 import { generateEmailBody, sendEmail } from "@/lib/nodemailer";
@@ -8,7 +13,7 @@ import { connectToDB } from "@/lib/scraper/mongoose";
 
 export const maxDuration = 59; // This function can run for a maximum of 300 seconds
 export const dynamic = "force-dynamic";
-export const revalidate = 0;
+export const revalidate = 3600;
 
 export async function GET(request: Request) {
   try {
@@ -61,9 +66,14 @@ export async function GET(request: Request) {
             url: updatedProduct.url,
           };
           // Construct emailContent
-          const emailContent = await generateEmailBody(productInfo, emailNotifType);
+          const emailContent = await generateEmailBody(
+            productInfo,
+            emailNotifType
+          );
           // Get array of user emails
-          const userEmails = updatedProduct.users.map((user: any) => user.email);
+          const userEmails = updatedProduct.users.map(
+            (user: any) => user.email
+          );
           // Send email notification
           await sendEmail(emailContent, userEmails);
         }
--- lib/scraper/mongoose.ts
+++ lib/scraper/mongoose.ts
@@ -4,12 +4,12 @@ let isConnected = false;// Variable to track connection status
 
 export const connectToDB = async ()=>{
     mongoose.set('strictQuery', true);
-    if(!'mongodb+srv://bhattparaj2:au54Hf5OAGzO6H7Q@cluster0.tektrp6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0') return console.log('MONGODB_URI is not defined');
+    if(!process.env.MONGODB_URI) return console.log('MONGODB_URI is not defined');
 
     if(isConnected) return console.log('=> using existing database connection');
  
     try{
-        await mongoose.connect('mongodb+srv://bhattparaj2:au54Hf5OAGzO6H7Q@cluster0.tektrp6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+        await mongoose.connect(process.env.MONGODB_URI);
         isConnected=true;
         console.log('MongoDB Connected');
     } catch(error){
